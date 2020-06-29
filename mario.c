49b3f26d553852a7e7c9c076aea2312950c00997
kay-kay-t <kay-kay-t@users.noreply.github.com>
2020-06-29 20:19:47 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -10,41 +10,41 @@ int main(void)
         n = get_int("Height: ");
     }
     while (n < 1 || n > 8);
-        // n = height
+    // n = height
 
-        if (n > 0 || n < 9)
+    if (n > 0 || n < 9)
+    {
+        int i = 0;
+        // i = counter
+
+        for (int j = 0; j < n; j++)
+            // j = row
         {
-            int i = 0;
-            // i = counter
-
-            for (int j = 0; j < n; j++)
-                // j = row
+            if (i != n)
+                // != - not equal
             {
-                if (i != n)
-                    // != - not equal
+                for (int k = (n - 1) - i; k > 0; k--)
+                    // k = spaces
                 {
-                    for (int k = (n - 1) - i; k > 0; k--)
-                        // k = spaces
-                    {
-                        printf(" ");
-                    }
-                    for (int m = 0; m <= i; m++)
-                        // m = hashes
-                    {
-                        printf("#");
-                    }
-
-                    printf("  ");
-
-                    for (int m = 0; m <= i; m++)
-                    {
-                        printf("#");
-                    }
-
-                    printf("\n");
-
-                    i++;
+                    printf(" ");
                 }
+                for (int m = 0; m <= i; m++)
+                    // m = hashes
+                {
+                    printf("#");
+                }
+
+                printf("  ");
+
+                for (int m = 0; m <= i; m++)
+                {
+                    printf("#");
+                }
+
+                printf("\n");
+
+                i++;
             }
         }
+    }
 }
