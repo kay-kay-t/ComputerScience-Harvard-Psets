757932f7f52045ae29f04c450ba3f72251ef9030
kay-kay-t <kay-kay-t@users.noreply.github.com>
2020-06-29 22:04:56 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -4,42 +4,43 @@
 
 
 int main(void)
-
-{   int c;
-    float n;     
- //promting for an answer, c - amount of coins, n - change owed
+{ 
+    int c
+    float n
+  
+//promting for an answer, c - amount of coins, n - change owed
     do
-   { 
-     n = get_float ("Change owed:");
-   }
+    { 
+        n = get_float("Change owed:");
+    }
     
-   while (n < 0);
+    while (n < 0);
    
-   n = n * 100;
-   n = round(n);
+    n = n * 100;
+    n = round(n);
    
-   while (n > 0)
-   {
-       if (n >= 25)
-       {
-           n = n - 25;
-           c = c + 1;
-       }
-       else if (n >= 10)
-       {
-           n = n - 10;
-           c = c + 1;
-       }
-       else if (n >= 5)
-       {
-           n = n - 5;
-           c = c + 1;
-       }
-       else if (n >= 1)
-       {
-           n = n - 1;
-           c = c + 1;
-       }
-   }
-   printf("Coins Used: %d\n", c);
+    while (n > 0)
+    {
+        if (n >= 25)
+        {
+            n = n - 25;
+            c = c + 1;
+        }
+        else if (n >= 10)
+        {
+            n = n - 10;
+            c = c + 1;
+        }
+        else if (n >= 5)
+        {
+            n = n - 5;
+            c = c + 1;
+        }
+        else if (n >= 1)
+        {
+            n = n - 1;
+            c = c + 1;
+        }
+    }
+    printf("Coins Used: %d\n", c);
 }   
