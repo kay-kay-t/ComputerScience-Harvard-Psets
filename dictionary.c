ed95a04fbffe92dadadadae4c91ebff4f9757e19
kay-kay-t <kay-kay-t@users.noreply.github.com>
2020-10-09 18:30:45 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -68,7 +68,7 @@ unsigned int hash(const char *word)
     int n;
     for (int i = 0; word[i] != '\0'; i++)
     {
-        if (toupper(isalpha(word[i])))
+        if (toupper(isalpha(word[i++])))
         {
             n = word[i] - 'a' + 1;
         }
