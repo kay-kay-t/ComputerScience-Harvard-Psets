683395050c37ca1ae90e1c55605ab4ff9f0fd419
kay-kay-t <kay-kay-t@users.noreply.github.com>
2020-10-20 18:29:37 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,15 +1,14 @@
 from cs50 import get_int
 while True:
-    # k - hight, j - width
-    k = get_int("Height: ")
-    j = k + 1
-    if k > 0 or k < 8:
-       break
-    # For each row  
-for i in range(0, k):
-    #Printing hashes
-    hash = i + 1
-    print("#" * hash)
-    #Printing spaces
-    space = j - hash
-    print("" * space, end="")
+    # - height, m - width 
+    n = get_int("Height: ")
+    m = n + 1
+    if n >= 1 and n <= 8:
+        break
+
+for i in range(0, n):
+    hashes = i + 1
+    spaces = m - hashes
+
+    print(" " * spaces, end="")
+    print("#" * hashes)
